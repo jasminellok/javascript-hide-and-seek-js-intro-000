bc0b9386a3a514d751424e818d641928f00403d0
Jasmine Lok <jasminellok@gmail.com>
2020-05-01 00:15:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ function increaseRankBy(n) {
   .querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString();
+    lis[i].innerHTML = parseInt(list[i].innerHTML)+n;
   }
+}
+
+
+function deepestChild() {
+  let currentNode = document.querySelector('#grand-node')
+  let nextNode = currentNode.children[0];
+  
+  while (nextNode) {
+    currentNode = nextNode;
+    nextNode = currentNode.children[0];
+  }
+  return (currentNode);
 }
